214c4ed696a8585cc966fe8e4f5a289ed5981ed4
tangbaoquyen97 <tangbaoquyen97@gmail.com>
2016-08-04 01:37:37 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<link rel="stylesheet" href="text.css">
+	<style>
+ table {
+    border-spacing: 100px;
+}
+</style>
+	
+</head>
+<body>
+	<table style ="width:50%">
+	<div class="container">
+		<h1><caption><a href="#">Southern region</a></caption></h1>
+		<tr>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event_one">
+						<div class="info"><th><span class="event-name"><img src="/images-QuyenTang/vietabroader.png" alt="VietAbroader" width="250" height="250">VietAbroader Career</span></th><span class="event-description"><td>From 2004 until now, by upholding our decade-proven values, VietAbroader has sent our members to numerous top universities and colleges around the world.</td></span></div>
+					</div>
+				</a>
+			</div>
+
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event_two">
+						<th><div class="info"><th><span class="event-name"><img src="/images-QuyenTang/aiesec.png" alt="aiesec" width="250" height="250">AIESEC Volunteering Opportunities</span></th><span class="event-description"><td>AIESEC is a global platform for young people to explore and develop their leadership potential.</td></span></div>
+					</div>
+				</a>
+			</div>
+			</tr>
+			</table>
+
+	<table style ="width:50%">
+	<div class="container">
+		<h1><caption><a href="#">Northern region</a></caption></h1>
+		<tr>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event_three">
+						<div class="info"><th><span class="event-name"><img src="/images-QuyenTang/v-simu.jpeg" alt="V-SIMU" width="250" height="250">CATAPULT</span></th><span class="event-description"><td>Vietnamese Students in MacEwan University's CATAPULT event.</td></span></div>
+					</div>
+				</a>
+			</div>
+
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event_two">
+						<th><div class="info"><th><span class="event-name"><img src="/images-QuyenTang/buildingbridges.jpeg" alt="BB" width="250" height="250">Building Bridges Volunteering Positions</span></th><span class="event-description"><td>Building Bridges is a project that provides free tutoring and mentoring for youth in Edmonton, Alberta, Canada</td></span></div>
+					</div>
+				</a>
+			</div>
+			</tr>
+			</table>
+			
+			
+
+			
+			</div>
+		</div>
+	</div>
+</body>
+</html>
